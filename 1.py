f9abf31ecad48ee340a08ec733c1524d3edc3dad
GergoV <geritwo@gmail.com>
2016-11-30 23:04:31 +0100

    1.py improved: Can process multiple parameters to comply with all scenarios.

--- 1.py
+++ 1.py
@@ -2,13 +2,13 @@
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
 
-def double_list_items(list_in):
+def double_list_items(list_in, *args): # Foolproof only if accepts multiple paramters too.
     list_out = []
     try:
         for i in list_in:
             list_out.append(i*2)
         return list_out
     except TypeError:
-        print('Error: argument is not a list.')
+        print('Error: Input parameter is not a list. Please input list.')
 
-print(double_list_items(1))
+print(double_list_items(1, 2))
