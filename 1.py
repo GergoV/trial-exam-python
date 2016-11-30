715c3ad335ec0e25683fefd4cd231e90d16cea1f
GergoV <geritwo@gmail.com>
2016-11-30 22:52:36 +0100

    Exercise 1.py complete.

--- 1.py
+++ 1.py
@@ -1,3 +1,14 @@
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
+
+def double_list_items(list_in):
+    list_out = []
+    try:
+        for i in list_in:
+            list_out.append(i*2)
+        return list_out
+    except TypeError:
+        print('Error: argument is not a list.')
+
+print(double_list_items(1))
