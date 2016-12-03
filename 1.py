b6e7a4128f1db3c9f85e1e2a0b0b696ab26891f4
GergoV <geritwo@gmail.com>
2016-12-03 23:32:37 +0100

    1.py corrected

--- 1.py
+++ 1.py
@@ -1,14 +1,15 @@
+
 # Create a function that takes a list as a parameter,
 # and returns a new list with all its element's values doubled.
 # It should raise an error if the parameter is not a list.
 
-def double_list_items(list_in, *args): # Foolproof only if accepts multiple paramters too.
+def double_list_items(list_in, *args): # Foolproof only if accepts multiple
     list_out = []
-    try:
+    if type(list_in) != list:
+        raise TypeError('Input paramter is not a list.')
+    else:
         for i in list_in:
             list_out.append(i*2)
         return list_out
-    except TypeError:
-        print('Error: Input parameter is not a list. Please input list.')
 
-print(double_list_items(1, 2))
+print(double_list_items([1, 2, 4]))
--- 2.py
+++ 2.py
@@ -1,3 +1,20 @@
 # Create a function that takes a filename as string parameter,
 # and counts the occurances of the letter "a", and returns it as a number.
 # It should not break if the file does not exist, just return 0.
+
+def count_a(input_filename):
+    try:
+        if type(input_filename) != str:
+            raise TypeError('Please input filename as string.')
+        else:
+            with open(input_filename) as f:
+                a_quant = 0
+                for i in input_filename:
+                    a_quant += 1
+                return a_quant
+
+    except FileNotFoundError:
+        return 99
+
+out = count_a('./gotcha.txt')
+print(out)
