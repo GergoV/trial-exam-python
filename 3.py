324224c40b4d239c5b5422e66de5cc3f36fd11f0
GergoV <geritwo@gmail.com>
2016-12-03 23:38:26 +0100

    3.py complete

--- 3.py
+++ 3.py
@@ -2,3 +2,16 @@
 # It should take its three sizes as constructor parameters (numbers)
 # It should have a method called `getSurface` that returns the cuboid's surface
 # It should have a method called `getVolume` that returns the cuboid's volume
+
+class MrCube:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def getSurface(self):
+        return 2*(x*y) + 2*(x*z) + 2*(y*z)
+
+    def getVolume(self):
+        return x*y*z
